fd0a53bcf033d246d10f0bad92bd0d5107b4afab
phamtanphat <phatdroid94@gmail.com>
2021-02-04 21:02:49 +0700

    function cong va tru

--- index.js
+++ index.js
@@ -27,19 +27,46 @@ const request = require("request");
 //     console.log(error || temp)
 // })
 
-function getTempCity(cityname){
+// function getTempCity(cityname){
+    // return new Promise((resolve , reject) => {
+    //     const url = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityname}`
+
+    //     request(url , {json : true}, function (error, response, body) {
+    //        if(error) return reject(error)
+    //        if(body.cod === "404") return reject(body.message)
+    //        return resolve(body.main.temp)
+    //     });
+    // })
+    
+// }
+
+// getTempCity("12312321")
+// .then(temp => console.log(temp))
+// .catch(error => console.log(error))
+
+function cong(a , b){
     return new Promise((resolve , reject) => {
-        const url = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityname}`
+        const url = `https://pheptinhonline.herokuapp.com/cong/${a}/${b}`
 
         request(url , {json : true}, function (error, response, body) {
            if(error) return reject(error)
-           if(body.cod === "404") return reject(body.message)
-           return resolve(body.main.temp)
+           if(!body.success) return reject(body.message)
+           return resolve(body.message)
         });
     })
-    
 }
 
-getTempCity("12312321")
-.then(temp => console.log(temp))
+function tru(a , b){
+    return new Promise((resolve , reject) => {
+        const url = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}`
+
+        request(url , {json : true}, function (error, response, body) {
+           if(error) return reject(error)
+           if(!body.success) return reject(body.message)
+           return resolve(body.message)
+        });
+    })
+}
+tru(1,1)
+.then(tong => console.log(tong))
 .catch(error => console.log(error))

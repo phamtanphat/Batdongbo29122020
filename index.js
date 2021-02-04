f03b24422f6f1276b40d4dda4961d4c1327159c0
phamtanphat <phatdroid94@gmail.com>
2021-02-04 20:40:43 +0700

    callback

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let a = 10
+// let a = 10
 
-handle(value => {
-    console.log(value)
-})
+// handle(value => {
+//     console.log(value)
+// })
 
-function handle(callback){
-    setTimeout(() => {
-        a = 5
-        callback(a)
-    },1000)
+// function handle(callback){
+//     setTimeout(() => {
+//         a = 5
+//         callback(a)
+//     },1000)
+// }
+const request = require("request");
+
+
+function getTempCity(cityname , callback){
+    const url = `http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=${cityname}`
+
+    request(url , {json : true}, function (error, response, body) {
+       if(error) return callback(error)
+       if(body.cod === "404") return callback(body.message)
+       return callback(null , body.main.temp)
+    });
 }
+
+getTempCity("Hanoi", (error , temp) => {
+    console.log(error || temp)
+})
